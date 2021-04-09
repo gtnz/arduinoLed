d8594cb90034b65da96ee884c1467485e05c526d
gtnz <gtnzgtmz@gmail.com>
2021-04-09 11:29:32 +0300

    Добавлена читаймость
    Добавил несколько комментариев и добавил #define

--- DigistumpTest.ino
+++ DigistumpTest.ino
@@ -6,7 +6,12 @@
 // Указываем, к какому порту подключен вход ленты DIN.
 #define LED_PIN 1
 
-#define DELAY 200
+// Задержка между переключениеми точек.
+#define DELAY 200 
+
+// Управляющие пины
+#define CONTROL_PIN1=0
+#define CONTROL_PIN2=4
  
 // Создаем переменную strip для управления нашей лентой.
 
@@ -18,7 +23,7 @@ void NextUp(uint8_t k) {
   for (int i = 0; i < LED_COUNT; i++) {
     strip[i] = CRGB::Black;
   }
-  if (k == LED_COUNT-1){
+  if (k == LED_COUNT-1){ 
     strip[k] = CRGB::Red;
     strip[0] = CRGB::Red;
     strip[1] = CRGB::Red;
@@ -63,37 +68,36 @@ void setup()
 {
   // Добавляем ленту
   FastLED.addLeds<WS2812B, LED_PIN, RGB>(strip, LED_COUNT);
-  pinMode(2, INPUT);
-  pinMode(0, INPUT);
-  pinMode(4, INPUT);
+  pinMode(CONTROL_PIN1, INPUT);
+  pinMode(CONTROL_PIN2, INPUT);
 }
  
 void loop()
 {
 //if (false) {
-  if (digitalRead(0) && !digitalRead(4)) {
+  if (digitalRead(CONTROL_PIN1) && !digitalRead(CONTROL_PIN2)) {
     if (n<=0)  
       n=LED_COUNT;
     n=n-1;
     NextDown(n);
     delay(DELAY);
   } else 
-    if (!digitalRead(0) && digitalRead(4)) {
+    if (!digitalRead(CONTROL_PIN1) && digitalRead(CONTROL_PIN2)) {
       if (n>=LED_COUNT-1)
         n=-1;
       n=n+1;
       NextUp(n);
       delay(DELAY);
     } else
-    if (!digitalRead(0) && !digitalRead(4)) {
-      for (int i = 0; i < LED_COUNT; i++) {
-        strip[i] = CRGB::Red;
-        FastLED.show();
-      }
-    } else
-      for (int i = 0; i < LED_COUNT; i++) {
-        strip[i] = CRGB::Green;
-        FastLED.show();
+      if (!digitalRead(CONTROL_PIN1) && !digitalRead(CONTROL_PIN2)) {
+        for (int i = 0; i < LED_COUNT; i++) {
+          strip[i] = CRGB::Red;
+          FastLED.show();
+        }
+      } else
+        for (int i = 0; i < LED_COUNT; i++) {
+          strip[i] = CRGB::Green;
+          FastLED.show();
       }
 //  } 
 }
